a6440a83922497b9c383304486bf66cac1a8ba83
micai-cloud <81576923+micai-cloud@users.noreply.github.com>
2022-12-03 18:54:40 +0800

    TEST10

--- telecom_live_lotterbak.py
+++ telecom_live_lotterbak.py
@@ -129,7 +129,7 @@ class TelecomLotter:
             active_code2 = self.get_action_id_other(liveId)
             if active_code1 is not None or active_code2 is not None:
                 break
-            print(f"此直播间暂无抽奖活动, 等待90秒后再次查询 剩余查询次数{7 - i}")
+            #print(f"此直播间暂无抽奖活动, 等待90秒后再次查询 剩余查询次数{7 - i}")
             await sleep(90)
             continue
         if active_code1 is None and active_code2 is None:
